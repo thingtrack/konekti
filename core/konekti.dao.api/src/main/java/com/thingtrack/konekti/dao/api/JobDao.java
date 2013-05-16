979e49bf6bc7fcb22b31b47f549b7596f67f9c4c
Miguel Salinas Gancedo <miguel@thingtrack.com>
2013-05-16 21:56:04 +0200

    correct bugfixes and add module map style

--- core/konekti.dao.api/src/main/java/com/thingtrack/konekti/dao/api/JobDao.java
+++ core/konekti.dao.api/src/main/java/com/thingtrack/konekti/dao/api/JobDao.java
@@ -1,8 +1,12 @@
 package com.thingtrack.konekti.dao.api;
 
+import java.util.List;
+
 import com.thingtrack.konekti.dao.template.Dao;
 import com.thingtrack.konekti.domain.Job;
+import com.thingtrack.konekti.domain.User;
 
 public interface JobDao extends Dao<Job, Integer> {
+	public List<Job> getAll(User user) throws Exception;
 	public Job getByGroupName(String group, String name) throws Exception;
 }
