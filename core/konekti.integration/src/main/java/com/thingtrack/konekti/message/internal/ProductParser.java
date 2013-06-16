b8b70014b8e3b837a6cd52cc3d914dac32aa8890
Miguel Salinas Gancedo <miguel@thingtrack.com>
2013-06-16 23:12:29 +0200

    add more integration classes

--- core/konekti.integration/src/main/java/com/thingtrack/konekti/message/internal/ProductParser.java
+++ core/konekti.integration/src/main/java/com/thingtrack/konekti/message/internal/ProductParser.java
@@ -1,17 +1,27 @@
 package com.thingtrack.konekti.message.internal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
+import com.thingtrack.konekti.domain.Area;
 import com.thingtrack.konekti.domain.Product;
+import com.thingtrack.konekti.domain.ProductUnit;
 import com.thingtrack.konekti.message.internal.domain.csv.product.CsvProduct;
+import com.thingtrack.konekti.service.api.AreaService;
 import com.thingtrack.konekti.service.api.ProductService;
 import com.thingtrack.konekti.service.api.ProductTypeService;
 import com.thingtrack.konekti.service.api.ProductUnitService;
+import com.thingtrack.konekti.service.api.SupplierService;
 
 public class ProductParser implements Processor {
+	@Autowired
+	private AreaService areaService;
+	
 	@Autowired
 	private ProductService productService;
 
@@ -21,31 +31,120 @@ public class ProductParser implements Processor {
 	@Autowired
 	private ProductUnitService productUnitService;
 	
+	@Autowired
+	private SupplierService supplierService;
+	
+	@SuppressWarnings("unchecked")
 	public void process(Exchange exchange) throws Exception {    	    	
-    	CsvProduct csvProduct = (CsvProduct)exchange.getIn().getBody();
+		List<CsvProduct> csvProducts = null;
+		
+		if (exchange.getIn().getBody() instanceof CsvProduct) {
+			csvProducts = new ArrayList<CsvProduct>();
+			
+			csvProducts.add((CsvProduct) exchange.getIn().getBody());
+		}
+		else
+    		csvProducts = (List<CsvProduct>) exchange.getIn().getBody();
     	
-    	// C: Create product
-    	// U: Update product
+    	// A: Create or Update product
     	// D: Delete product
-    	if (csvProduct.getAction() != null && csvProduct.getAction().equals("C")) {
-		 productService.save(parse(csvProduct));
-	 	}
-	 	else if (csvProduct.getAction() != null &&  csvProduct.getAction().equals("U")) {
-			productService.save(parse(csvProduct));
-	 	}
-	 	else if (csvProduct.getAction() != null &&  csvProduct.getAction().equals("D")) {
-			Product product = parse(csvProduct);
-		 	//product.setActive(false);
-		 
-		 	productService.save(product);
-	 	}
-    	    	    	
-        //System.out.println("We just downloaded: " + exchange.getIn().getHeader("CamelFileName"));
+    	for (CsvProduct csvProduct : csvProducts) {
+	    	if (csvProduct.getAction().equals("A"))
+	    		productService.save(actualize(csvProduct));
+	   	 	else if (csvProduct.getAction().equals("D"))
+	   			productService.save(remove(csvProduct));
+	       	   
+    	}
+
     }
     
-    private Product parse(CsvProduct csvProduct) throws Exception {
-    	return null;
+    private Product actualize(CsvProduct csvProduct) throws Exception {
+    	Area area = areaService.getByCode(csvProduct.getAreaCode());
+    	    	    	
+    	Product product = null;
+    	try {
+    		if (csvProduct.getVersion() != null)
+    			product = productService.getByCode(csvProduct.getCode(), csvProduct.getVersion());
+    		else
+    			product = productService.getByCode(csvProduct.getCode());
+    		
+        	if (!containsArea(product, csvProduct.getAreaCode()))
+        		product.getAreas().add(area);
+    	}
+    	catch(Exception e) {
+        	product = productService.createNewEntity(area);
+    	}
     	
+    	// product setters
+    	product.setCode(csvProduct.getCode());
+    	product.setName(csvProduct.getName());
+    	product.setVersion(csvProduct.getVersion());
+    	product.setDescription(csvProduct.getDescription());
+    	product.setProductType(productTypeService.getByCode(csvProduct.getTypeCode()));
+    	product.setHasLote(parseBoolean(csvProduct.getHasLote()));
+    	product.setHasSerialNumber(parseBoolean(csvProduct.getHasSerialNumber()));
+    	product.setHasExpeditionDate(parseBoolean(csvProduct.getHasExpeditionDate()));
+    	product.setAveragePrice(Float.parseFloat(csvProduct.getAveragePrice()));
+    	product.setProductSupplier(supplierService.getByCode(csvProduct.getSupplierCode()));
+    	if (!containsProductUnit(product, csvProduct.getProductUnitCode()))
+    		product.getProductUnits().add(productUnitService.getByCode(csvProduct.getProductUnitCode()));
+    	product.setProductActive(parseBoolean(csvProduct.getProductActive()));
+    	
+    	return product;
+    	
+    }
+    
+    private Product remove(CsvProduct csvProduct) throws Exception {
+    	Area area = areaService.getByCode(csvProduct.getAreaCode());
+    	
+    	Product product = null;
+    	try {
+    		if (csvProduct.getVersion() != null)
+    			product = productService.getByCode(csvProduct.getCode(), csvProduct.getVersion());
+    		else
+    			product = productService.getByCode(csvProduct.getCode());
+    	}
+    	catch(Exception e) {
+        	throw e;
+        	
+    	}
+    	
+    	// product setters
+    	if (csvProduct.getAreaCode().equals("*"))
+    		product.setProductActive(false);
+    	
+    	if (containsArea(product, csvProduct.getAreaCode()))
+    		product.getAreas().remove(area);
+    	
+    	if (containsProductUnit(product, csvProduct.getProductUnitCode()))
+    		product.getProductUnits().remove(productUnitService.getByCode(csvProduct.getProductUnitCode()));
+    	
+    	return product;
+    }
+    
+    private boolean parseBoolean(String value) {
+    	if (value.equals("0"))
+    		return false;
+    	else
+    		return true;
+    }
+    
+    private boolean containsArea(Product product, String areaCode) {
+    	for (Area area : product.getAreas()) {
+    		if (area.getCode().equals(areaCode))
+    			return true;
+    		
+    	}
+    	return false;
+    }
+    
+    private boolean containsProductUnit(Product product, String productUnitCode) {
+    	for (ProductUnit productUnit : product.getProductUnits()) {
+    		if (productUnit.getCode().equals(productUnitCode))
+    			return true;
+    		
+    	}
+    	return false;
     }
 }
 
--- core/konekti.integration/src/main/java/com/thingtrack/konekti/message/internal/domain/csv/product/CsvProduct.java
+++ core/konekti.integration/src/main/java/com/thingtrack/konekti/message/internal/domain/csv/product/CsvProduct.java
@@ -7,16 +7,49 @@ import org.apache.camel.dataformat.bindy.annotation.DataField;
 
 @SuppressWarnings("serial")
 @CsvRecord(separator=";")
-public class CsvProduct implements Serializable {	
-	@DataField(pos = 1)
+public class CsvProduct implements Serializable  {	
+	@DataField(pos = 1, required = true)
 	private String action;
 		
-	@DataField(pos = 2)
-	private String productCode;
+	@DataField(pos = 2, required = true)
+	private String code;
 	
-	@DataField(pos = 3)
+	@DataField(pos = 3, required = true)
+	private String name;
+	
+	@DataField(pos = 4)
+	private String version;
+	
+	@DataField(pos = 5, required = true)
 	private String description;
 
+	@DataField(pos = 6, required = true)
+	private String typeCode;
+	
+	@DataField(pos = 7, required = true)
+	private String hasLote;
+	
+	@DataField(pos = 8, required = true)
+	private String hasSerialNumber;
+	
+	@DataField(pos = 9, required = true)
+	private String hasExpeditionDate;
+	
+	@DataField(pos = 10, precision = 2)
+	private String averagePrice;
+	
+	@DataField(pos = 11, required = true)
+	private String supplierCode;
+	
+	@DataField(pos = 12, required = true)
+	private String productUnitCode;
+	
+	@DataField(pos = 13, required = true)
+	private String areaCode;
+	
+	@DataField(pos = 14, required = true)
+	private String productActive;
+	
 	/**
 	 * @return the action
 	 */
@@ -32,17 +65,17 @@ public class CsvProduct implements Serializable {
 	}
 	
 	/**
-	 * @return the productCode
+	 * @return the code
 	 */
-	public String getProductCode() {
-		return productCode;
+	public String getCode() {
+		return code;
 	}
 
 	/**
-	 * @param productCode the productCode to set
+	 * @param code the code to set
 	 */
-	public void setProductCode(String productCode) {
-		this.productCode = productCode;
+	public void setCode(String code) {
+		this.code = code;
 	}
 
 	/**
@@ -57,5 +90,159 @@ public class CsvProduct implements Serializable {
 	 */
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the version
+	 */
+	public String getVersion() {
+		return version;
+	}
+
+	/**
+	 * @param version the version to set
+	 */
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	/**
+	 * @return the typeCode
+	 */
+	public String getTypeCode() {
+		return typeCode;
+	}
+
+	/**
+	 * @param typeCode the typeCode to set
+	 */
+	public void setTypeCode(String typeCode) {
+		this.typeCode = typeCode;
+	}
+
+	/**
+	 * @return the hasLote
+	 */
+	public String getHasLote() {
+		return hasLote;
+	}
+
+	/**
+	 * @param hasLote the hasLote to set
+	 */
+	public void setHasLote(String hasLote) {
+		this.hasLote = hasLote;
+	}
+
+	/**
+	 * @return the hasSerialNumber
+	 */
+	public String getHasSerialNumber() {
+		return hasSerialNumber;
+	}
+
+	/**
+	 * @param hasSerialNumber the hasSerialNumber to set
+	 */
+	public void setHasSerialNumber(String hasSerialNumber) {
+		this.hasSerialNumber = hasSerialNumber;
+	}
+
+	/**
+	 * @return the hasExpeditionDate
+	 */
+	public String getHasExpeditionDate() {
+		return hasExpeditionDate;
+	}
+
+	/**
+	 * @param hasExpeditionDate the hasExpeditionDate to set
+	 */
+	public void setHasExpeditionDate(String hasExpeditionDate) {
+		this.hasExpeditionDate = hasExpeditionDate;
+	}
+
+	/**
+	 * @return the averagePrice
+	 */
+	public String getAveragePrice() {
+		return averagePrice;
+	}
+
+	/**
+	 * @param averagePrice the averagePrice to set
+	 */
+	public void setAveragePrice(String averagePrice) {
+		this.averagePrice = averagePrice;
+	}
+
+	/**
+	 * @return the supplierCode
+	 */
+	public String getSupplierCode() {
+		return supplierCode;
+	}
+
+	/**
+	 * @param supplierCode the supplierCode to set
+	 */
+	public void setSupplierCode(String supplierCode) {
+		this.supplierCode = supplierCode;
+	}
+
+	/**
+	 * @return the productActive
+	 */
+	public String getProductActive() {
+		return productActive;
+	}
+
+	/**
+	 * @param productActive the productActive to set
+	 */
+	public void setProductActive(String productActive) {
+		this.productActive = productActive;
+	}
+
+	/**
+	 * @return the areaCode
+	 */
+	public String getAreaCode() {
+		return areaCode;
+	}
+
+	/**
+	 * @param areaCode the areaCode to set
+	 */
+	public void setAreaCode(String areaCode) {
+		this.areaCode = areaCode;
+	}
+
+	/**
+	 * @return the productUnitCode
+	 */
+	public String getProductUnitCode() {
+		return productUnitCode;
+	}
+
+	/**
+	 * @param productUnitCode the productUnitCode to set
+	 */
+	public void setProductUnitCode(String productUnitCode) {
+		this.productUnitCode = productUnitCode;
 	}
 }
